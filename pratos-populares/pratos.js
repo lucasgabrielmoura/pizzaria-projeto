3bb4cad96a73383600e6db0cb1ed6bb9bf3d68c4
lucasgabrielmoura <lucasgabrieldemoura.2021@gmail.com>
2021-10-10 20:35:58 -0300

    tela-de-pratos-e-cardapio

---
+++ pratos-populares/pratos.js
@@ -0,0 +1,41 @@
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carroselUp(){
+    idx++;
+
+    if(idx>img.length - 1){
+        idx = 0;
+    }
+    imgs.style.transform = `translateX(${-idx * 21}vw)`;
+    if(idx == 0 || idx == 1){
+        document.querySelector(".pizza1").classList.toggle("active")
+    }
+    if(idx == 1 || idx == 2){
+        document.querySelector(".pizza2").classList.toggle("active")
+    }
+    if(idx == 2 || idx == 0){
+        document.querySelector(".pizza3").classList.toggle("active")
+    }
+}
+function carroselDown(){
+    idx--;
+
+    if(idx<img.length - img.length){
+        idx = img.length - 1;
+    }
+    imgs.style.transform = `translateX(${-idx * 21}vw)`;
+    if(idx == 0 || idx == 2){
+        document.querySelector(".pizza1").classList.toggle("active")
+    }
+    if(idx == 1 || idx == 0){
+        document.querySelector(".pizza2").classList.toggle("active")
+    }
+    if(idx == 2 || idx == 1){
+        document.querySelector(".pizza3").classList.toggle("active")
+    }
+}
+
+setInterval(carroselUp,5000)
---
+++ tela-de-cardapio/cardapio.js
@@ -0,0 +1,27 @@
+function active(){
+    document.querySelector(".imagem1").classList.toggle("active");
+}
+function active2(){
+    document.querySelector(".imagem2").classList.toggle("active");
+}
+function active3(){
+    document.querySelector(".imagem3").classList.toggle("active");
+}
+function active4(){
+    document.querySelector(".imagem4").classList.toggle("active");
+}
+function active5(){
+    document.querySelector(".imagem5").classList.toggle("active");
+}
+function active6(){
+    document.querySelector(".imagem6").classList.toggle("active");
+}
+function active7(){
+    document.querySelector(".imagem7").classList.toggle("active");
+}
+function active8(){
+    document.querySelector(".imagem8").classList.toggle("active");
+}
+function active9(){
+    document.querySelector(".imagem9").classList.toggle("active");
+}

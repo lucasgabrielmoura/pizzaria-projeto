e31edd2d20cc613d942cf51f0ea33adf1cff3d74
Henrique <henriquefernandes.gouveia@gmail.com>
2021-10-11 15:05:03 -0300

    Tudo pronto

--- Contato/contato.js
+++ Contato/contato.js
@@ -1,36 +1,36 @@
    
     var x = document.getElementById("enviar");
     function modal() {
-    let nome = document.getElementById("txtn").value;
-    let email = document.getElementById("txte").value;
-    let telefone = document.getElementById("numbert").value;
-    let mensagem = document.getElementById("mensagem").value;
+        let nome = document.getElementById("txtn").value;
+        let email = document.getElementById("txte").value;
+        let telefone = document.getElementById("numbert").value;
+        let mensagem = document.getElementById("mensagem").value;
 
     /* Validador de email */
-    let testRegex = function () {  
-    let regex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
-        return regex.test(email);
+        let testRegex = function () {  
+        let regex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+            return regex.test(email);
     } 
 
     
     /* Validador para saber se todos os campos estão preenchidos corretamente */
-    if(nome == "" || email == "" || telefone == "" || mensagem == "") {
-        let testando = document.getElementById("msg");
-        let modal = document.querySelector(".modal");
-        modal.classList.toggle("active");
-            testando.innerText = "Por favor verifique se todos os dados foram preechidos.";
-    } else if(testRegex() === true) {
-        let testando = document.getElementById("msg");
-        let modal = document.querySelector(".modal");
-        modal.classList.toggle("active");
-        testando.innerText = "Sua mensagem foi enviada a Magic Pizzaria!";
-        document.getElementById("botao").setAttribute("href","/Home/index.html");
-        } else {
-            let testando = document.getElementById("msg");
+        if(nome == "" || email == "" || telefone == "" || mensagem == "") {
+            let msgModal = document.getElementById("msg");
             let modal = document.querySelector(".modal");
             modal.classList.toggle("active");
-            testando.innerText = "Email inválido";
-        }
+            msgModal.innerText = "Por favor verifique se todos os dados foram preechidos.";
+        } else if(testRegex() === true) {
+            let msgModal = document.getElementById("msg");
+            let modal = document.querySelector(".modal");
+            modal.classList.toggle("active");
+            msgModal.innerText = "Sua mensagem foi enviada a Magic Pizzaria!";
+            document.getElementById("botao").setAttribute("href","/Home/index.html");
+            } else {
+                let msgModal = document.getElementById("msg");
+                let modal = document.querySelector(".modal");
+                modal.classList.toggle("active");
+                msgModal.innerText = "Email inválido";
+            }
     }
 
     

fa36331b3e57d595b2532bc224767abec66853c4
Henrique <henriquefernandes.gouveia@gmail.com>
2021-10-10 23:55:58 -0300

    ajustando

--- Home/script.js
+++ Home/script.js
@@ -1,11 +1,8 @@
-function modal(){
-    var x = "";
-    x = document.getElementById("newUni").href;
-    console.log(x)
-    if(x.endsWith("")){
-        let pagina = document.getElementById("msg")
-        pagina.innerText = "Está pagina ainda não foi criada.";
-    } else {
-        window.location.href = "http://127.0.0.1:5500/index.html"
+ f/* unction modal(){
+    let x = "";
+    x = document.getElementById("newUni").getAttribute("href");
+    if(x == "#"){
+        let testando = document.getElementById("msg")
+        testa
     }
-}
+}  */

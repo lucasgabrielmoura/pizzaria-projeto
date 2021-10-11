1568713d302bb5e47907233e7839417a7fac4bdf
PedroGabrielll <pedrogabriel4545@hotmail.com>
2021-10-11 10:56:39 -0300

    Atualização JS

--- Reserva/reserva.js
+++ Reserva/reserva.js
@@ -7,10 +7,13 @@ function validador() {
     let dataHora = document.getElementById("dataHora").value 
     let quantidade = document.getElementById("quantidade").value
 
+
     if (name == ""|| email == "" || telefone == "" || dataHora == "" || quantidade == ""){
-        document.getElementById("modal").innerText = "Por favor verifique se todos os dados foram preenchidos."
+        let modal = document.querySelector(".modal");
+        modal.classList.toggle("active");
+        let p = document.getElementById("mensagemtext").innerText = "Por favor verifique se todos os dados foram preechidos."
     }else{
-        document.getElementById("modal").innerText = "Sua Reserva foi salva, estamos te aguardando na Magic Pizzaria!"
+        document.getElementById("mensagemtext").innerHTML = "Sua Reserva foi salva, estamos te aguardando na Magic Pizzaria!"
     }
 }
 // Função caso a reserva seja cancelada

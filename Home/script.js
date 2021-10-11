afece51a9b2c5e5c7b3c3251df740bd89eac1c54
lucasgabrielmoura <lucasgabrieldemoura.2021@gmail.com>
2021-10-10 23:53:14 -0300

    contato-home

--- Home/script.js
+++ Home/script.js
@@ -1,21 +1,3 @@
-/* function modal(){
-    var x = "";
-    x = document.getElementById("newUni").href;
-    console.log(x)
-    if(x.endsWith("#")){
-        let pagina = document.getElementById("msg")
-        let modal = document.querySelector(".modal");
-        modal.StyleSheet.display = 'block';
-        pagina.innerText = "Está pagina ainda não foi criada.";
-    } 
-} */
-
-
 function modal() {
-    test = document.getElementById("testando").getAttribute("href");
-    let modal = document.querySelector(".modal");
-        
-    if(test == "#") {
-        modal.classList.toggle('active');
-    }
+    document.querySelector(".modal").classList.toggle("active")
 }
